6c28471f65c9aea28b27cb5abbf61fd6b9b0c7c8
Phạm Minh Tuấn <phamminhtuan22072005sv@gmail.com>
2026-01-26 13:56:10 +0700

    test lib

--- DTSU666/src/main.cpp
+++ DTSU666/src/main.cpp
@@ -2,13 +2,22 @@
 #include <stdint.h>
 #include "dtsu666.h"
 #include "ModbusMaster.h"
+#include "LiquidCrystal_I2C.h"
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup(){
     Serial.begin(9600);
     Init_dtsu666();
+
+    lcd.init();
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    lcd.print("Xin chao!");
 }
 
 void loop(){
+    /*
     Serial.print("Ua:");
     Serial.println(Read_Voltage_A());
     Serial.print("Ub:");
@@ -16,4 +25,6 @@ void loop(){
     Serial.print("Uc:");
     Serial.println(Read_Voltage_C());
     delay(5000);
+    */
+
 }
